b9eb111ebb2201b6424f33ebd2656a50ca84986d
hasenradball <mail@frankhaefele.de>
2025-05-14 12:19:43 +0200

    update example

--- examples/sma_modbus_2mariaDB.py
+++ examples/sma_modbus_2mariaDB.py
@@ -4,13 +4,13 @@
 
 import mysql.connector
 from mysql.connector import errorcode
-from sma_modbus import SunnyBoy as sunny_boy
+from sma_modbus import SunnyTripower as sunny_tripower
 from mariadb_config import MARIA_DB_CONFIG
 from maria_db_mysql import MariaDBMysql as maria_db
 
 # Main
 if __name__ == "__main__":
-    sunny_obj = sunny_boy("192.168.178.29")
+    sunny_obj = sunny_tripower("192.168.178.29")
     power_total = sunny_obj.get_active_power()[0]
     i1_ac = sunny_obj.get_ac_current()[0]
 
